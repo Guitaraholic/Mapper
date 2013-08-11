13cbf357b8ac0fb88a97c13bc9bb93a6f860256a
Paul McDonough <paul.mcdonough@virginmedia.co.uk>
2013-08-11 18:14:16 +0100

    updates to icon and navbar

--- appserver/static/application.js
+++ appserver/static/application.js
@@ -2,5 +2,4 @@ if(Splunk.Module.AppBar) { Splunk.Module.AppBar.prototype.bindEventListeners = f
 $(function(){
 	$('.auxLinks a.aboutLink').remove();
 	$('.auxLinks span').remove();
-    $('.auxLinks a.help').attr({ title: 'About Splunk for Google Maps', href: Splunk.util.make_url("app","maps","about") }).html('About Google Maps for Splunk');
 });
